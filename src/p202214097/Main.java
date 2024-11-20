95eecd62d8846b5bc4babbcd2e1ea57fe7d9c5f7
6202-15 <6202-15@6202-15>
2024-11-20 10:04:50 +0900

    six

--- src/p202214097/Main.java
+++ src/p202214097/Main.java
@@ -1,16 +1,16 @@
 package p202214097;
 
+import static java.lang.System.out;
+
 public class Main {
 	public static void main(String[] arge) {
-		System.out.println("202214097  정은지");
-		System.out.println(Hello.sum(10));
-		System.out.println(Hello.sum(100));
-		System.out.println(Hello.sum(1000));
-		System.out.println(World.factorial(3));
-		System.out.println(World.factorial(6));
-		System.out.println(World.factorial(9));
-		System.out.println(Sansu.max(new int[] {8, 5, 6, 9, 4, 1, 7, 10, 3, 2, 8}));
-		System.out.println(Sansu.max(new int[] {-2, 5, 6, 9, -6, 1, 7, 2, 8, 10}));
+		out.println("202214097 정은지");
+		out.println(Hello.sum(10));
+		out.println(Hello.sum(100));
+		out.println(Hello.sum(1000));
+		out.println(Sansu.max(new int[] {8, 5, 6, 9, 4, 1, 7, 10, 3, 2, 8}));
+		out.println(Sansu.max(new int[] {-2, 5, 6, 9, -6, 1, 7, 2, 8, 10}));
+
 
 	}
 }
