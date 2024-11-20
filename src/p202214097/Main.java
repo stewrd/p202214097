e35630fcc54a2d7d6838875f92ea2d286358fb0d
6202-15 <6202-15@6202-15>
2024-11-20 09:53:45 +0900

    three

--- src/p202214097/Main.java
+++ src/p202214097/Main.java
@@ -5,5 +5,6 @@ public class Main {
 		System.out.println("202214097 정은지");
 		System.out.println(Hello.sum(10));
 		System.out.println(Hello.sum(100));
+		System.out.println(Hello.sum(1000));
 	}
 }
