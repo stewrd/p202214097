c31a01b75435ccbaa0f08359e78d59fb1c49a5fe
6202-15 <6202-15@6202-15>
2024-11-20 09:51:16 +0900

    two

---
+++ src/p202214097/Hello.java
@@ -0,0 +1,10 @@
+package p202214097;
+
+public class Hello {
+	public static int sum(int n) {
+		int result = 0;
+		for (int i = 1; i <= n; ++i)
+				result += i;
+		return result;
+	}
+}
--- src/p202214097/Main.java
+++ src/p202214097/Main.java
@@ -3,5 +3,7 @@ package p202214097;
 public class Main {
 	public static void main(String[] arge) {
 		System.out.println("202214097 정은지");
+		System.out.println(Hello.sum(10));
+		System.out.println(Hello.sum(100));
 	}
 }
